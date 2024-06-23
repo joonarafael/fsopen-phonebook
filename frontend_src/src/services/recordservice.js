0e5ba3f411956b054ded9c9da4d76daee97ad00d
Joona Kettunen <joona.kettunen21@gmail.com>
2024-06-23 16:49:08 +0300

    tired from this bs

--- frontend_src/src/services/recordservice.js
+++ frontend_src/src/services/recordservice.js
@@ -1,22 +1,24 @@
 import axios from "axios";
 
+const BASE_URL = "https://fsopen-phonebook-hdvt.onrender.com/api/persons";
+
 const getAll = () => {
-	const request = axios.get(process.env.BASE_URL);
+	const request = axios.get(BASE_URL);
 	return request.then((response) => response.data.persons);
 };
 
 const create = (newObject) => {
-	const request = axios.post(process.env.BASE_URL, newObject);
+	const request = axios.post(BASE_URL, newObject);
 	return request.then((response) => response.data);
 };
 
 const remove = (id) => {
-	const request = axios.delete(`${process.env.BASE_URL}/${id}`);
+	const request = axios.delete(`${BASE_URL}/${id}`);
 	return request.then((response) => response.data);
 };
 
 const update = (id, newObject) => {
-	const request = axios.put(`${process.env.BASE_URL}/${id}`, newObject);
+	const request = axios.put(`${BASE_URL}/${id}`, newObject);
 	return request.then((response) => response.data);
 };
 
